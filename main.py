f16f8eb7cdf12da2bae2feaaf1683e4cea639947
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-06-12 09:43:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def send_page(user_id, first_name, chat_id,
     page_number, page_url= get_page(page_number, is_start)
     next_button = button(text="▶️Halaman berikutnya", callback_data=f"{page_number + 1} {user_id} {first_name}")\
                     if with_markup else None
-    back_button = button(text="◀️Halaman Sebelumnya"", callback_data=f"{page_number - 1} {user_id} {first_name}")\
+    back_button = button(text="◀️Halaman Sebelumnya", callback_data=f"{page_number - 1} {user_id} {first_name}")\
                     if with_markup else None
     start_button = button(text="Pembukaan Al-Qur'an🕋", callback_data=f"{1} {user_id} {first_name}")\
                     if with_markup else None
